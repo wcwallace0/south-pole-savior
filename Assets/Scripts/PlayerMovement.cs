edc8ce1dc3ccdb3b935085468404e93d99a1bdb1
Will Wallace <wwallac1@trinity.edu>
2024-10-22 21:24:41 -0500

    Implemented basic spline movement with constant velocity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,63 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Transform playerTransform;
     public float jumpForce = 400f;
     // public GameObject diagonal;
+    
+    private bool onSpline = false;
 
+    public SplineContainer spline;
+    private float splineLength;
+    private float speed;
     public float maxVelocity = 5f;
     public float accelRate = 0.5f;
-    
-    private bool onSlope = false;
+    private float distancePercentage = 0f;
+
+    private void Start() {
+        splineLength = spline.CalculateLength();
+
+        // Enter spline
+        onSpline = true;
+        rb.gravityScale = 0;
+    }
+
+    private void FixedUpdate() {
+        if(onSpline) {
+            distancePercentage += speed * Time.deltaTime / splineLength;
+            playerTransform.position = spline.EvaluatePosition(distancePercentage);
+            Accelerate();
+        }
+    }
+
+    private void Accelerate() {
+        // TODO determine angle of slope and apply force (acceleration) accordingly
+        // apply less acceleration the closer you are to maxVelocity? if so, wouldn't need if statement below
+        speed += accelRate;
+
+        if(speed > maxVelocity) {
+            speed = maxVelocity;
+        } else if(speed < -maxVelocity) {
+            speed = -maxVelocity;
+        }
+    }
+
+    private void EnterSpline() {
+        onSpline = true;
+
+        // TODO check each position along the spline at some interval (hopefully not too small)
+        // get the position closest to the player position and snap the player to that
+        // you can stop iterating once the distance starts decreasing?
+    }
+
+    private void ExitSpline() {
+        onSpline = false;
+    }
 
     // private void FixedUpdate() {
     //     if(onSlope) {
