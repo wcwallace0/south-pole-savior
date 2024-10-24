f942f8b1d008ae9e9c83d8fd6a3354fed91f78ab
Will Wallace <wwallac1@trinity.edu>
2024-10-23 19:40:04 -0500

    Created basic test level, boosting, and skidding

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,65 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float maxVelocity;
+    public float boostSpeed;
     public float jumpForce;
+    public float dragWhenStopping;
     public GroundCheck gc;
 
-    private bool onSlope;
+    public float xVelocity;
+
+    private bool isFacingRight = true;
 
     private void Start() {
-        rb.velocity = new Vector3(7f, 0f, 0f);
+        // rb.velocity = new Vector3(-7f, 0f, 0f);
     }
 
     private void Update() {
-        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
+        // Enforce max x velocity
+        if(rb.velocity.x > maxVelocity) {
+            rb.velocity = new Vector2(maxVelocity, rb.velocity.y);
+        } else if(rb.velocity.x < -maxVelocity) {
+            rb.velocity = new Vector2(-maxVelocity, rb.velocity.y);
+        }
 
-        if(gc.isGrounded && Input.GetKeyDown(KeyCode.Space)) {
+        // for display in inspector
+        xVelocity = rb.velocity.x;
+
+        // Jump
+        if(gc.isGrounded && Input.GetKeyDown(KeyCode.Mouse0)) {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        // Direction
+        if(Input.GetKeyDown(KeyCode.D) && !isFacingRight) {
+            // face right
+            isFacingRight = true;
+        } else if(Input.GetKeyDown(KeyCode.A) && isFacingRight) {
+            // face left
+            isFacingRight = false;
+        }
+
+        // Slow player down when they hold in direction opposite of movement
+        if((rb.velocity.x > 0 && Input.GetKey(KeyCode.A)) || (rb.velocity.x < 0 && Input.GetKey(KeyCode.D))) {
+            rb.drag = dragWhenStopping;
+        } else {
+            rb.drag = 0;
+        }
+
+        // Ducking
+        if(Input.GetKey(KeyCode.S)) {
+            // TODO duck
+        }
+
+        // Boost
+        if(Input.GetKeyDown(KeyCode.Mouse1)) {
+            Vector2 boost = rb.velocity;
+            if(isFacingRight) {
+                boost.x = boostSpeed;
+            } else {
+                boost.x = -boostSpeed;
+            }
+            rb.velocity = boost;
         }
     }
 }
