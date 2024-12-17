53443decdfb61b87471723110ca96350772e2e31
Colin Houston <chousto1@trinity.edu>
2024-12-16 20:04:58 -0600

    Defeat/victory screens now function, except for continue button on victory screen which doesn't do anything yet

--- Assets/Scripts/Bossfight/LoadGame.cs
+++ Assets/Scripts/Bossfight/LoadGame.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadGame : MonoBehaviour
 {
@@ -11,6 +13,8 @@ public class LoadGame : MonoBehaviour
     public PlayerActions player;
     public Cybersecurity cybersec;
     public Alert alert;
+
+    private int clickCt;
 
     
     // Start is called before the first frame update
@@ -58,5 +62,42 @@ public class LoadGame : MonoBehaviour
         } else{
             SceneManager.LoadScene("Cyberfight Defeat");
         }
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Cyberfight");
+    }
+    public void QuitButton()
+    {
+        clickCt ++;
+        if (clickCt == 2)
+        {
+            SceneManager.LoadScene("Menu");
+        } else if (clickCt == 1)
+        {
+            StartCoroutine(QuitTimer());
+        }
+    }
+
+    IEnumerator QuitTimer()
+    {
+        Button quit = GameObject.Find("Button_Quit").GetComponent<Button>();
+        TextMeshProUGUI quitText = GameObject.Find("Text_Quit").GetComponent<TextMeshProUGUI>();
+        Image buttImage = quit.GetComponent<Image>();
+
+
+        string currText = quitText.text;
+        Color currColor = buttImage.color;
+
+        quitText.text = "Are you sure?";
+        quit.GetComponent<Image>().color = Color.red;
+
+        yield return new WaitForSeconds(5f);
+
+        quitText.text = currText;
+        buttImage.color = currColor;
+        
+
     }
 }
